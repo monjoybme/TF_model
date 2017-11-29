bb07ce183bf89c45c13fb5ef2cffa07807f1cb6f
pavelkrolevets <pavelkrolevets@gmail.com>
2017-11-29 18:48:26 +0800

    new file:   Model.py 	new file:   Resize_and_split.py 	new file:   build_image_data.py

---
+++ Resize_and_split.py
@@ -0,0 +1,88 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""
+This code is the preparation of your own images to feed them into build_image_data.py to create TFrecords file
+"""
+
+import os
+import sys
+from random import shuffle
+from pathlib import Path
+import shutil
+from PIL import Image
+
+new_size = 200 # dimentions to resize raw imgs - means 200x200. Smaller images will be stretched.
+
+imageFolder_raw = '/home/pavelkrolevets/Working/TF_facenet/data/raw/' # adr to raw imgs
+imageFolder_train = '/home/pavelkrolevets/Working/TF_facenet/data/TRAIN_DIR/' # adr to train folder to store resized imgs
+imageFolder_test = '/home/pavelkrolevets/Working/TF_facenet/data/VALIDATION_DIR/'# adr to test folder to store resized imgs
+
+def resize(folder, fileName, Size):
+    filePath = os.path.join(folder, fileName)
+    im = Image.open(filePath)
+    w, h  = im.size
+    newIm = im.resize((Size, Size))
+    # overrider orginal, you can save a copy
+    newIm.save(filePath)
+
+def bulkResize(Folder, factor):
+    imgExts = ["png", "bmp", "jpg"]
+    for path, dirs, files in os.walk(Folder):
+        for fileName in files:
+            ext = fileName[-3:].lower()
+            if ext not in imgExts:
+                continue
+
+            resize(path, fileName, factor)
+
+# split data to train and test
+
+dir_list=[x[0] for x in os.walk(imageFolder_raw)]
+dir_list = dir_list[1:]
+just_dirs = next(os.walk(imageFolder_raw))[1]
+print(just_dirs)
+for i in just_dirs:
+    f = []
+    imgExts = ["png", "bmp", "jpg"]
+    for path, dirs, files in os.walk(imageFolder_raw+i):
+        for fileName in files:
+            ext = fileName[-3:].lower()
+            if ext not in imgExts:
+                continue
+            f.append(fileName)
+            shuffle(f) # DONT FORGET TO SHUFFLE :-)
+        train_files = f[0:int(0.8 * len(f))]
+        test_files = f[int(0.8 * len(f)):]
+        print(train_files, '\n', test_files)
+
+    # creating folders
+    if not os.path.exists(imageFolder_train+i+'/'):
+        os.makedirs(imageFolder_train+i+'/')
+    if not os.path.exists(imageFolder_test+i+'/'):
+        os.makedirs(imageFolder_test+i+'/')
+
+    #copying files
+    for file in train_files:
+        print(imageFolder_raw + i + '/' + file)
+        print(imageFolder_train+i+'/')
+        shutil.copy2(imageFolder_raw+i+'/'+file, imageFolder_train+i+'/')
+    for file in test_files:
+        print(imageFolder_raw + i + '/' + file)
+        print(imageFolder_test + i + '/')
+        shutil.copy2(imageFolder_raw + i + '/' + file, imageFolder_test + i + '/')
+
+# resize
+bulkResize(imageFolder_train, new_size)
+bulkResize(imageFolder_test, new_size)
